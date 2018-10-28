99b6b3cb699033c6938c727cf8969cfa15bcd112
nkidmano <nobudy55@gmail.com>
2018-10-28 12:41:54 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from './heroes';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { Hero } from './hero.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-pagination';
+export class AppComponent implements OnInit {
+  heroes = HEROES;
+  heroesPagination: Hero[];
+
+  ngOnInit(): void {
+    this.heroesPagination = this.heroes.slice(0, 5);
+  }
+
+  pageChanged(event: PageChangedEvent): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    this.heroesPagination = this.heroes.slice(startItem, endItem);
+  }
 }
+
+
